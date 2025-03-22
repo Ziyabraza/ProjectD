694ce2b995ac9ea5d78609841da50dcc8ef993e6
elcapodeicapi <159782576+elcapodeicapi@users.noreply.github.com>
2025-03-23 00:02:58 +0100

    s

--- obj/Debug/net9.0/ProjectD.AssemblyInfo.cs
+++ obj/Debug/net9.0/ProjectD.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e2231e652bd1a9415bbcc2fdf57598903693f54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65fddac619392163aa4e17356ba56e854f086db1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
