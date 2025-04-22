21c7eb4cdede82e81516dfee9f1215e7c7463dd4
elcapodeicapi <hudayfa2005@gmail.com>
2025-04-22 11:42:48 +0200

    yes

--- ProjectD.Tests/UnitTest1.cs
+++ ProjectD.Tests/UnitTest1.cs
@@ -1,10 +1,10 @@
-﻿namespace ProjectD.Tests;
+﻿// namespace ProjectD.Tests;
 
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
+// public class UnitTest1
+// {
+//     //[Fact]
+//     public void Test1()
+//     {
 
-    }
-}
+//     }
+// }
