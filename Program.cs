f41aa00b1557cde06c0c7169ce48c276ed0df84b
ZiyabRaza001 <chanda.zr@gmail.com>
2025-06-01 02:36:58 +0200

    add openAPI weggehaald en dotnet8 gezet

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ var builder = WebApplication.CreateBuilder(new WebApplicationOptions
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
